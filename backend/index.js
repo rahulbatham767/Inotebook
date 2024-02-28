df972bb26318bbb710eb261e1299fdd7e2bbffae
rahulbatham767 <rahulbatham767@gmail.com>
2024-02-29 00:23:12 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ var cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // or an array of allowed origins
+    origin: "https://inotebook-frontend-three.vercel.app", // or an array of allowed origins
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // include credentials like cookies
   })
